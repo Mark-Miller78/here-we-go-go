2066cc2f1823f2645f89e986b28a0f70b927e7ce
Mark Miller <markmiller7858@gmail.com>
2025-02-23 12:32:48 -0500

    throw error and working with Slice

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,12 +1,47 @@
 package greetings
 
 import (
+	"errors"
 	"fmt"
+	"math/rand"
 )
 
 // Hello returns a greeting from the named person
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	//if no name was give, return an error with message
+	if name == "" {
+		return "", errors.New("empty name")
+	}
 	//Return a greeting that embeds the name in the message
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// Hellos returns a map that associates each of the named people with a greeting message
+func Hellos(names []string) (map[string]string, error) {
+	//a map to associate names with the messages.
+	messages := make(map[string]string)
+	//Loop through the recieved slice of names, calling the Hello function to get a message for each name
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+		//in the map, associate the retrieved message with the name
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+// randomFormat returns on of a set of greeting messages. The returned message is selected at random
+func randomFormat() string {
+	//a slice of message formats
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	//Return a randomly selected message format by specifying a random index for the slice of formats
+	return formats[rand.Intn(len(formats))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -3,10 +3,27 @@ package main
 import (
 	"fmt"
 	"here-we-go-go/greetings"
+	"log"
 )
 
 func main() {
-	//Get a greeting message and print it
-	message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	//A slice of names
+	names := []string{"mark", "kevin", "carter"}
+
+	//request greeting messages for the names
+	messages, err := greetings.Hellos(names)
+
+	//if error was returned, print it to the consol and exit program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//if no error was returned, print the returned message to the console
+	fmt.Println(messages)
 }
